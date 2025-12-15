c67c50d7b429e08fa465e88f147be36ddb75840b
akncnkoc <akincankoc@gmail.com>
2025-12-15 14:07:59 +0300

    dropbox silindi update

---
+++ lib/models/app_models.dart
@@ -0,0 +1,32 @@
+import 'dart:typed_data';
+import 'crop_data.dart';
+
+class OpenPdfTab {
+  final String pdfPath;
+  final String title;
+  final String? dropboxPath;  // Also used for Google Drive with 'gdrive:' prefix
+  final CropData? cropData;
+  final String? zipFilePath;
+  final Uint8List? pdfBytes;
+  final Uint8List? zipBytes;
+
+  OpenPdfTab({
+    required this.pdfPath,
+    required this.title,
+    this.dropboxPath,
+    this.cropData,
+    this.zipFilePath,
+    this.pdfBytes,
+    this.zipBytes,
+  });
+}
+
+class BreadcrumbItem {
+  final String name;
+  final String path;
+
+  BreadcrumbItem({
+    required this.name,
+    required this.path,
+  });
+}
